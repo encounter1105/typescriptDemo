15476a3bc42ad34c5e721a7942c41171ead9afd5
encounter1105 <1459722320@qq.com>
2020-12-01 17:40:42 +0800

    update

---
+++ src/Demo15.ts
@@ -0,0 +1,17 @@
+// "use strict";
+// strict属性如果设置为true,就代表我们的编译和书写规范，要按照TypeScript最严格的规范来写
+// var person = "taiji";
+// noImplicitAny属性的作用是，允许你的注解类型 any 不用特意表明
+
+// function taiji(name :any ) {
+//     return name
+// }
+
+// strictNullChecks 设置为false,它的意思就是，不强制检查 NULL 类型
+
+// const taiji:string  = null
+
+// noUnusedLocals和noUnusedParameters 
+
+// const taiji: string = null;
+// export const name = "taiji";
---
+++ src/Demo16.ts
@@ -0,0 +1,52 @@
+interface Waiter {
+    anjiao: boolean;
+    say: () => {};
+  }
+  
+  interface Teacher {
+    anjiao: boolean;
+    skill: () => {};
+  }
+
+//   类型断言的方式
+//   function judgeWho(animal: Waiter | Teacher) {
+//     //   animal.say() 
+//     if (animal.anjiao) {
+//         (animal as Teacher).skill();
+//       }else{
+//         (animal as Waiter).say();
+//       }
+//   }
+
+//   in语法
+
+// function judgeWhoTwo(animal: Waiter | Teacher) {
+//     if ("skill" in animal) {
+//       animal.skill();
+//     } else {
+//       animal.say();
+//     }
+//   }
+
+// typeOf方法
+// function add(first: string | number, second: string | number) {
+//     // return first + second;
+//     if (typeof first === "string" || typeof second === "string") {
+//         return `${first}${second}`;
+//       }
+//       return first + second;
+//   }
+
+//  instanceOf 语法
+class NumberObj {
+    count: number;
+}
+function addObj(first: object | NumberObj, second: object | NumberObj) {
+    // return first.count + second.count;
+    if (first instanceof NumberObj && second instanceof NumberObj) {
+        return first.count + second.count;
+    }
+    return 0;
+}
+
+
